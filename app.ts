020700f521bf8b316c795aa2bac66ef83603443a
Anatoliy Yakovenko <yakovenko.anatoliy@gmail.com>
2023-06-26 18:18:51 +0300

    start task 2

--- app.ts
+++ app.ts
@@ -1,11 +1,27 @@
-const button = document.querySelector("button");
-const input1 = document.getElementById("num1")! as HTMLInputElement;
-const input2 = document.getElementById("num2")! as HTMLInputElement;
+// TASK 1
 
-function add(num1: number, num2: number) {
-  return num1 + num2;
-}
+// const button = document.querySelector("button");
+// const input1 = document.getElementById("num1")! as HTMLInputElement;
+// const input2 = document.getElementById("num2")! as HTMLInputElement;
 
-button.addEventListener("click", function() {
-  console.log(add(+input1.value, +input2.value));
-});
+// function add(num1: number, num2: number) {
+//   return num1 + num2;
+// }
+
+// button.addEventListener("click", function() {
+//   console.log(add(+input1.value, +input2.value));
+// });
+
+// TASK 2
+
+let age = 50;
+let name1 = 'Max';
+let toggle = true;
+let empty = null;
+let notInitialize = undefined;
+let callback = (a:number):number => { return 100 + a };
+
+let anything: any;
+anything = 'Text';
+anything = {};
+
