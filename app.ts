9274820c8e06c504eb4c1e4b19ab29f91656fcc1
Anatoliy Yakovenko <yakovenko.anatoliy@gmail.com>
2023-06-26 18:38:17 +0300

    complete Task2

--- app.ts
+++ app.ts
@@ -25,3 +25,73 @@ let anything: any;
 anything = 'Text';
 anything = {};
 
+let some:unknown;
+some = 'Text';
+let str: string;
+if(typeof some === 'string'){
+  str = some;
+}
+
+let person:[string,number]
+ person = ['Max', 21];
+
+ enum Load {LOADING,READY}
+ const page = {
+  load: Load.READY
+}
+
+if (page.load === Load.LOADING) {
+  console.log('Сторінка завантажується');
+}
+if (page.load === Load.READY) {
+  console.log('Сторінка завантажена');
+}
+
+ let val: string | number;
+
+ let literal: 'enable' | 'disable';
+
+ function showMessage(message:string): void {
+  console.log(message);
+}
+
+
+function calc(num1:number, num2:number): number {
+  return num1 + num2;
+}
+
+function customError(): never {
+  throw new Error('Error');
+}
+
+type PageDate={
+  title: string,
+  likes: number,
+  accounts: string[],
+  status: 'open' | 'close',
+  details?: {
+    createAt: Date,
+    updateAt: Date,
+  }
+}
+
+
+const page1: PageDate = {
+  title: 'The awesome page',
+  likes: 100,
+  accounts: ['Max', 'Anton', 'Nikita'],
+  status: 'open',
+  details: {
+    createAt: new Date('2021-01-01'),
+    updateAt: new Date('2021-05-01'),
+  }
+}
+
+const page2: PageDate = {
+  title: 'Python or Js',
+  likes: 5,
+  accounts: ['Alex'],
+  status: 'close',
+}
+
+
