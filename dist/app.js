f9cad17e52835ab7385a79e004ababb7d8394154
Anatoliy Yakovenko <yakovenko.anatoliy@gmail.com>
2023-07-03 11:15:06 +0300

    task3 completed

--- dist/app.js
+++ dist/app.js
@@ -1,15 +1,4 @@
 "use strict";
-// TASK 1
-// const button = document.querySelector("button");
-// const input1 = document.getElementById("num1")! as HTMLInputElement;
-// const input2 = document.getElementById("num2")! as HTMLInputElement;
-// function add(num1: number, num2: number) {
-//   return num1 + num2;
-// }
-// button.addEventListener("click", function() {
-//   console.log(add(+input1.value, +input2.value));
-// });
-// TASK 2
 let age = 50;
 let name1 = 'Maxx';
 let toggle = true;
@@ -68,3 +57,4 @@ const page2 = {
     accounts: ['Alex'],
     status: 'close',
 };
+//# sourceMappingURL=app.js.map
