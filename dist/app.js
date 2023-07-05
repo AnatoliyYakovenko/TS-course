ef6b58ff5ef63846faa3e72eecf62a03dd332b6d
Anatoliy Yakovenko <yakovenko.anatoliy@gmail.com>
2023-07-05 12:03:50 +0300

    work with OOP

--- dist/app.js
+++ dist/app.js
@@ -1,60 +1,15 @@
 "use strict";
-let age = 50;
-let name1 = 'Maxx';
-let toggle = true;
-let empty = null;
-let notInitialize = undefined;
-let callback = (a) => { return 100 + a; };
-let anything;
-anything = 'Text';
-anything = {};
-let some;
-some = 'Text';
-let str;
-if (typeof some === 'string') {
-    str = some;
-}
-let person;
-person = ['Max', 21];
-var Load;
-(function (Load) {
-    Load[Load["LOADING"] = 0] = "LOADING";
-    Load[Load["READY"] = 1] = "READY";
-})(Load || (Load = {}));
-const page = {
-    load: Load.READY
-};
-if (page.load === Load.LOADING) {
-    console.log('Сторінка завантажується');
-}
-if (page.load === Load.READY) {
-    console.log('Сторінка завантажена');
-}
-let val;
-let literal;
-function showMessage(message) {
-    console.log(message);
-}
-function calc(num1, num2) {
-    return num1 + num2;
-}
-function customError() {
-    throw new Error('Error');
-}
-const page1 = {
-    title: 'The awesome page',
-    likes: 100,
-    accounts: ['Max', 'Anton', 'Nikita'],
-    status: 'open',
-    details: {
-        createAt: new Date('2021-01-01'),
-        updateAt: new Date('2021-05-01'),
+class A {
+    constructor() {
+        this.someProperty = "str";
     }
-};
-const page2 = {
-    title: 'Python or Js',
-    likes: 5,
-    accounts: ['Alex'],
-    status: 'close',
-};
+}
+class B extends A {
+    showProperty() {
+        console.log(this.someProperty);
+    }
+}
+const a = new A();
+const b = new B();
+b.showProperty();
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -14,84 +14,101 @@
 
 // TASK 2
 
-let age = 50;
-let name1 = 'Maxx';
-let toggle = true;
-let empty = null;
-let notInitialize = undefined;
-let callback = (a:number):number => { return 100 + a };
+// let age = 50;
+// let name1 = 'Maxx';
+// let toggle = true;
+// let empty = null;
+// let notInitialize = undefined;
+// let callback = (a:number):number => { return 100 + a };
 
-let anything: any;
-anything = 'Text';
-anything = {};
+// let anything: any;
+// anything = 'Text';
+// anything = {};
 
-let some:unknown;
-some = 'Text';
-let str: string;
-if(typeof some === 'string'){
-  str = some;
+// let some:unknown;
+// some = 'Text';
+// let str: string;
+// if(typeof some === 'string'){
+//   str = some;
+// }
+
+// let person:[string,number]
+//  person = ['Max', 21];
+
+//  enum Load {LOADING,READY}
+//  const page = {
+//   load: Load.READY
+// }
+
+// if (page.load === Load.LOADING) {
+//   console.log('Сторінка завантажується');
+// }
+// if (page.load === Load.READY) {
+//   console.log('Сторінка завантажена');
+// }
+
+//  let val: string | number;
+
+//  let literal: 'enable' | 'disable';
+
+//  function showMessage(message:string): void {
+//   console.log(message);
+// }
+
+
+// function calc(num1:number, num2:number): number {
+//   return num1 + num2;
+// }
+
+// function customError(): never {
+//   throw new Error('Error');
+// }
+
+// type PageDate={
+//   title: string,
+//   likes: number,
+//   accounts: string[],
+//   status: 'open' | 'close',
+//   details?: {
+//     createAt: Date,
+//     updateAt: Date,
+//   }
+// }
+
+
+// const page1: PageDate = {
+//   title: 'The awesome page',
+//   likes: 100,
+//   accounts: ['Max', 'Anton', 'Nikita'],
+//   status: 'open',
+//   details: {
+//     createAt: new Date('2021-01-01'),
+//     updateAt: new Date('2021-05-01'),
+//   }
+// }
+
+// const page2: PageDate = {
+//   title: 'Python or Js',
+//   likes: 5,
+//   accounts: ['Alex'],
+//   status: 'close',
+// }
+
+class A{
+  protected someProperty ="str"
 }
 
-let person:[string,number]
- person = ['Max', 21];
-
- enum Load {LOADING,READY}
- const page = {
-  load: Load.READY
-}
-
-if (page.load === Load.LOADING) {
-  console.log('Сторінка завантажується');
-}
-if (page.load === Load.READY) {
-  console.log('Сторінка завантажена');
-}
-
- let val: string | number;
-
- let literal: 'enable' | 'disable';
-
- function showMessage(message:string): void {
-  console.log(message);
-}
-
-
-function calc(num1:number, num2:number): number {
-  return num1 + num2;
-}
-
-function customError(): never {
-  throw new Error('Error');
-}
-
-type PageDate={
-  title: string,
-  likes: number,
-  accounts: string[],
-  status: 'open' | 'close',
-  details?: {
-    createAt: Date,
-    updateAt: Date,
+class B extends A{
+  showProperty(){
+    console.log(this.someProperty);
+    
   }
 }
+const a = new A();
+const b = new B();
 
+// a.someProperty;
+// b.someProperty;
 
-const page1: PageDate = {
-  title: 'The awesome page',
-  likes: 100,
-  accounts: ['Max', 'Anton', 'Nikita'],
-  status: 'open',
-  details: {
-    createAt: new Date('2021-01-01'),
-    updateAt: new Date('2021-05-01'),
-  }
-}
-
-const page2: PageDate = {
-  title: 'Python or Js',
-  likes: 5,
-  accounts: ['Alex'],
-  status: 'close',
-}
-
+b.showProperty();
 
