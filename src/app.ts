f8921c929a82552f9d8fb3bd36c3723b4fb03eb0
Anatoliy Yakovenko <yakovenko.anatoliy@gmail.com>
2023-07-08 14:29:49 +0300

    getters and setters

--- src/app.ts
+++ src/app.ts
@@ -142,51 +142,55 @@
 //     this.type = type;
 //   }
 // }
-class House{
-  private tenants: string[]=[];
-
-  constructor(private readonly type: string, private street: string) {}
-
-  public showAddress(this: House){
-  console.log('Address:' +this.street);
-  }
-  public showType(this: House){
-    console.log('House Type:' + this.type);
-  }
-  public addTenant(tenant: string){
-    this.tenants.push(tenant);
-  }
-  public showTenants(){
-
-    console.log(this.tenants);
-  }
-}
-
-class StoneHouse extends House{
- private chargeOfTheHouse: string;
-
- constructor(street: string, generalTalent: string){
-   super('stone', street);
-   this.chargeOfTheHouse = generalTalent;
-   this.addTenant(generalTalent);
- }
-public showTenants(): void {
-  console.log('General:' + this.chargeOfTheHouse);
-  super.showTenants();
-}
-}
-const stoneHouse = new StoneHouse('Stone-world', 'Max');
-
-stoneHouse.addTenant("Alina");
-
-stoneHouse.addTenant("Tolya");
-
-stoneHouse.addTenant("Juliya");
 
 
-stoneHouse.showTenants();
-stoneHouse.showType();
-stoneHouse.showAddress();
+//*************INHERITANCE*********** */
+
+// class House{
+//   private tenants: string[]=[];
+
+//   constructor(private readonly type: string, private street: string) {}
+
+//   public showAddress(this: House){
+//   console.log('Address:' +this.street);
+//   }
+//   public showType(this: House){
+//     console.log('House Type:' + this.type);
+//   }
+//   public addTenant(tenant: string){
+//     this.tenants.push(tenant);
+//   }
+//   public showTenants(){
+
+//     console.log(this.tenants);
+//   }
+// }
+
+// class StoneHouse extends House{
+//  private chargeOfTheHouse: string;
+
+//  constructor(street: string, generalTalent: string){
+//    super('stone', street);
+//    this.chargeOfTheHouse = generalTalent;
+//    this.addTenant(generalTalent);
+//  }
+// public showTenants(): void {
+//   console.log('General:' + this.chargeOfTheHouse);
+//   super.showTenants();
+// }
+// }
+// const stoneHouse = new StoneHouse('Stone-world', 'Max');
+
+// stoneHouse.addTenant("Alina");
+
+// stoneHouse.addTenant("Tolya");
+
+// stoneHouse.addTenant("Juliya");
+
+
+// stoneHouse.showTenants();
+// stoneHouse.showType();
+// stoneHouse.showAddress();
 
 
 
